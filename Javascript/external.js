91037d5c64bd6632685e91b3e3eb670880e6646b
CampbellTxState <campbelltxstate@gmail.com>
2025-09-30 17:17:29 -0500

    Lab 04 Problems1

--- Javascript/external.js
+++ Javascript/external.js
@@ -24,12 +24,12 @@
 // document.getElementById("copyright").innerHTML = "Copyright &copy; " + year;
 function welcome()
 {
-    let a = "Please enter your name.";
-    let b = "Type your name here.";
+let a = "Please enter your name.";
+let b = "Type your name here.";
 // A prompt box is used to prompt users to input a value before entering a page.
-    user_name = window.prompt(a, b);
-    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
-    return message
+user_name = window.prompt(a, b);
+let message = "<h1>Hello, welcome to my webpage, " + user_name + "!<h1>"
+return message
 }
-document.write(welcome());
-document.write(message);
+document.getElementById("greeting").textContent = welcome()
+document.(message);
